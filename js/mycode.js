27a437caa4d02139c74a5f272f3e090b3e70df6b
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-02-02 22:16:50 +0800

    Implement refreshing task list

---
+++ js/mycode.js
@@ -0,0 +1,14 @@
+var apiBaseUrl = "http://127.0.0.1:10010";
+
+function getTasks() {
+    $.ajax({
+        type: 'GET',
+        url: apiBaseUrl + "/tasks",
+    }).done(function(data) { // data is the body of returned JSON object
+        var output = "";
+        for (var i = 0; i < data.length; i++) {
+            output += data[i].task_id + " " + data[i].task_description + " " + data[i].status + "\n";
+        }
+        $("#tasks").val(output);
+    });
+}
